bd9ee545d40aa1f6fe878bd70ff1227327a268c5
AJ Speller <ajspeller@gmail.com>
2019-07-07 19:26:54 -0400

    Added to person route

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ const express = require('express');
 const debug = require('debug')('app:index');
 const chalk = require('chalk');
 const morgan = require('morgan');
+const path = require('path');
 
 const personRouter = require('./routes/person');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(morgan('dev'));
-app.use(express.static('public'));
+app.use((req, res, next) => {
+  debug(`${new Date().toString()} -- ${req.originalUrl}`);
+  next();
+});
 app.use(express.json());
 app.use(
   express.urlencoded({
@@ -18,7 +21,18 @@ app.use(
   })
 );
 
+app.use(morgan('dev'));
+app.use(express.static('public'));
 app.use(personRouter);
+
+app.use((req, res, next) => {
+  res.status(404).send('Resourse not found');
+  next();
+});
+
+app.use((err, req, res, next) => {
+  res.senFile(path.join(__dirname, '../public/500.html'));
+});
 
 app.listen(PORT, () => {
   debug(`Webserver started on port ${chalk.inverse.bold.green(PORT)}`);
--- src/routes/person.js
+++ src/routes/person.js
@@ -3,7 +3,16 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/person', (req, res) => {
+  const { name } = req.query;
+  if (name) {
+    return res.send(`Person requested: ${name}`);
+  }
   res.send('Person requested');
+});
+
+router.get('/person/:name', (req, res) => {
+  const { name } = req.params;
+  res.send(`You have requested ${name}`);
 });
 
 module.exports = router;
